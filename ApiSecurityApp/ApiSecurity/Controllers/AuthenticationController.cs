0cc22a58e3cbe2779419742231507d2b5710c88c
Pieter B <pieterbeernink@hotmail.com>
2022-08-25 10:29:22 +0200

    complete token generation functionality

--- ApiSecurityApp/ApiSecurity/Controllers/AuthenticationController.cs
+++ ApiSecurityApp/ApiSecurity/Controllers/AuthenticationController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace ApiSecurity.Controllers;
 
@@ -7,18 +11,67 @@ namespace ApiSecurity.Controllers;
 [ApiController]
 public class AuthenticationController : ControllerBase
 {
+    private readonly IConfiguration _config;
+
     // HARD CODED LOGIN DETAILS SOLUTION - FOR QUICK TESTING ONLY
     public record AuthenticationData(string? UserName, string? Password);
     public record UserData(int UserId, string UserName);
+
+    public AuthenticationController(IConfiguration config)
+    {
+        _config = config;
+    }
 
     // api/Authentication/token
     [HttpPost("token")]
     public ActionResult<string> Authenticate([FromBody] AuthenticationData data)
     {
         // validate credentials
-        
+        var user = ValidateCredentials(data);
+
+        if (user is null)
+        {
+            return Unauthorized();
+        }
+
+        // generate token
+        var token = GenerateToken(user);
+
+        return Ok(token);
+    }
+
+    private string GenerateToken(UserData user)
+    {
+        // Encoding converts the given secret string into an ASCII byte array
+        // which is then passed into to SymmetricSecurityKey, to create a new instance
+        // Note that : is used to navigate one level down under Authentication (in secrets.json) to grap SecretKey
+        var secretKey = new SymmetricSecurityKey(
+            Encoding.ASCII.GetBytes(
+                _config.GetValue<string>("Authentication:SecretKey")));
+
+        // Signature for token
+        // If the signature doesn't match anymore, it means the token has been tampered with
+        var signingCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+
+        // Add claims: datapoints about the user, to be verified
+        List<Claim> claims = new();
+        claims.Add(new(JwtRegisteredClaimNames.Sub, user.UserId.ToString())); // verify the subject is the user
+        claims.Add(new(JwtRegisteredClaimNames.UniqueName, user.UserName)); // verify the username is the user
+
+        // Build token
+        var token = new JwtSecurityToken(
+            _config.GetValue<string>("Authentication:Issuer"),
+            _config.GetValue<string>("Authentication:Audience"),
+            claims,
+            DateTime.UtcNow, // when token becomes valid (as of right now)
+            DateTime.UtcNow.AddMinutes(1), // when token expires
+            signingCredentials: signingCredentials); // signing of token, to ensure it cannot be modified without becoming invalid
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
     }
         
+    
+    // Dummy lookup
     private UserData ValidateCredentials(AuthenticationData data)
     {
         // NOT PRODUCTION CODE - FOR DEMO ONLY
