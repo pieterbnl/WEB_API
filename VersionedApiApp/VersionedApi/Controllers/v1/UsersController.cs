4ed38516453d70e7711e8264fd2f1bf5021da46a
Pieter B <pieterbeernink@hotmail.com>
2022-09-01 12:50:39 +0200

    split controller in multiple versions and decorate as such

---
+++ VersionedApiApp/VersionedApi/Controllers/v1/UsersController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace VersionedApi.Controllers.v1;
+
+[Route("api/v{version:apiVersion}/[controller]")]
+[ApiController]
+[ApiVersion("1.0")]    
+public class UsersController : ControllerBase
+{
+    // GET: api/v1/Users
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+        return new string[] { "V1 value1", "V2 value2" };
+    }
+
+    // GET api/<UsersController>/5
+    [HttpGet("{id}")]
+    public string Get(int id)
+    {
+        return "value";
+    }
+
+    // POST api/<UsersController>
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+    }
+
+    // PUT api/<UsersController>/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
+
+    // DELETE api/<UsersController>/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
+}
---
+++ VersionedApiApp/VersionedApi/Controllers/v2/UsersController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace VersionedApi.Controllers.v2;
+
+[Route("api/v{version:apiVersion}/[controller]")]
+[ApiController]
+[ApiVersion("2.0")]
+public class UsersController : ControllerBase
+{
+    // GET: api/v2/Users
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+        return new string[] { "V2 value1", "V2 value2" };
+    }
+
+    // GET api/<UsersController>/5
+    [HttpGet("{id}")]
+    public string Get(int id)
+    {
+        return "value";
+    }
+
+    // POST api/<UsersController>
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+    }
+
+    // PUT api/<UsersController>/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
+
+    // DELETE api/<UsersController>/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
+}
