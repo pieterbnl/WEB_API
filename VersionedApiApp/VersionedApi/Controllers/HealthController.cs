8b111d0f810fd52bb8c1d732befade1951f58523
Pieter B <pieterbeernink@hotmail.com>
2022-09-01 17:12:42 +0200

    try setting a neutral controller

---
+++ VersionedApiApp/VersionedApi/Controllers/HealthController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VersionedApi.Controllers;
+
+[Route("api/v{version:apiVersion}/[controller]")]
+[ApiController]
+[ApiVersionNeutral] // applies to everything in the controller:
+                    // no matter what version you ask for, you get the same controller
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    [Route("ping")]
+    public IActionResult Ping()
+    {
+        return Ok("Pong");
+    }
+
+}
