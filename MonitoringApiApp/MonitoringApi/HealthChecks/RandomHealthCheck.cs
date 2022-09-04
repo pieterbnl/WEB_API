50b3fe6f4116de53ef8baa6bf3e4cf72de5a2701
Pieter B <pieterbeernink@hotmail.com>
2022-09-04 22:46:52 +0200

    write a health check and implement MS healthcheck UI

---
+++ MonitoringApiApp/MonitoringApi/HealthChecks/RandomHealthCheck.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace MonitoringApi.HealthChecks;
+
+public class RandomHealthCheck : IHealthCheck
+{
+    // Async call to check the health of the application
+    public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context,
+        CancellationToken cancellationToken = default)
+    {
+        // Health check example: check simulated response time
+        int responseTimeInMs = Random.Shared.Next(300);
+        
+        if (responseTimeInMs < 100)
+        {
+            return Task.FromResult(HealthCheckResult.Healthy(
+                $"Response time is {responseTimeInMs} ms"));
+        }
+        else if (responseTimeInMs <200)
+        {
+            return Task.FromResult(HealthCheckResult.Degraded(
+                $"Response time is {responseTimeInMs} ms"));
+        }
+        else
+        {
+            return Task.FromResult(HealthCheckResult.Unhealthy(
+                $"Response time is {responseTimeInMs} ms"));
+        }
+    }
+}
--- MonitoringApiApp/MonitoringApi/Program.cs
+++ MonitoringApiApp/MonitoringApi/Program.cs
@@ -1,8 +1,22 @@
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using MonitoringApi.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHealthChecks()
+    .AddCheck<RandomHealthCheck>("Site health check")
+    .AddCheck<RandomHealthCheck>("Db health check");
+
+builder.Services.AddHealthChecksUI(opts =>
+{
+    opts.AddHealthCheckEndpoint("api", "/health"); // api monitoring itself via /health - note: can also point to multiple url's
+    opts.SetEvaluationTimeInSeconds(5); // carrying out a health check every 5 seconds
+    opts.SetMinimumSecondsBetweenFailureNotifications(10); // only send notifications every 10 seconds upon failure - note: typically, 5 minutes or so is a good value
+}).AddInMemoryStorage(); // store logging in memory, for demonstration purposes
 
 var app = builder.Build();
 
@@ -18,5 +32,12 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapHealthChecks("/health", new HealthCheckOptions
+{ 
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse // tells how to format data that its gotten
+});
+
+app.MapHealthChecksUI();
 
 app.Run();
