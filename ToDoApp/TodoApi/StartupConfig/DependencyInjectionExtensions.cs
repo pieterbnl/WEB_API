8a973d9b959b3f61d688624e7a557ea18fb64446
Pieter B <pieterbeernink@hotmail.com>
2022-11-23 22:21:07 +0100

    move builder.services actions to seperate class

--- ToDoApp/TodoApi/StartupConfig/DependencyInjectionExtensions.cs
+++ ToDoApp/TodoApi/StartupConfig/DependencyInjectionExtensions.cs
@@ -1,6 +1,44 @@
-﻿namespace TodoApi.StartupConfig
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using TodoLibrary.DataAccess;
+
+namespace TodoApi.StartupConfig;
+
+public static class DependencyInjectionExtensions
 {
-    public class DependencyInjectionExtensions
+    public static void AddServices(this WebApplicationBuilder builder)
     {
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddSingleton<ISqlDataAccess, SqlDataAccess>();
+        builder.Services.AddSingleton<ITodoData, TodoData>();
+
+        builder.Services.AddAuthorization(opts =>
+        {
+            opts.FallbackPolicy = new AuthorizationPolicyBuilder()
+                .RequireAuthenticatedUser()
+                .Build();
+        });
+
+        builder.Services.AddHealthChecks()
+            .AddSqlServer(builder.Configuration.GetConnectionString("Default"));
+
+        builder.Services.AddAuthentication("Bearer")
+            .AddJwtBearer(opts =>
+            {
+                opts.TokenValidationParameters = new()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = builder.Configuration.GetValue<string>("Authentication:Issuer"),
+                    ValidAudience = builder.Configuration.GetValue<string>("Authentication:Audience"),
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.ASCII.GetBytes(
+                        builder.Configuration.GetValue<string>("Authentication:SecretKey")))
+                };
+            });
     }
 }
